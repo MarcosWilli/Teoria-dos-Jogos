b07e354c918693fae7b7a8c4f20d75ad3ca5f104
Marcos <marcoswillireiscampos@gmail.com>
2018-12-08 14:46:56 -0200

    Equilíbrios de Nash do primeiro jogo
    Ainda falta resolver o problema do 'idxmax' que retorna somente a primeira estratégia maximizadora (o problema ocorre quando duas estratégias levam ao payoff máximo)

--- Jog2_T1_Fin_Pur.py
+++ Jog2_T1_Fin_Pur.py
@@ -1,44 +1,73 @@
 import pandas as pd
 import numpy as np
 
-def to_string(x):
-    t = '{}'.format(x)
-    return t
-
-def JG2_T1_EP_EF(J1E, J2E, menor, maior, partial='jogo'):
+def estrategias_payoffs(J1E, J2E, menor, maior):
     '''
     Parâmetros:
     - J1E: Número de estratégias do jogador 1
     - J2E: Número de estratégias do jogador 2
     - menor: valor mínimo do payoff
     - maior: valor máximo do payoff
-    - partial: ['J1', 'J2']
-    ''' 
+    
+    Retorna a representação (DataFrame) dos payoffs de um jogador. A função deve ser chamada duas vezes para obter os dois DataFrames.
+    '''
+    
+    # Criação dos nomes das colunas e das linhas (estratégias):
     linhas = list(range(1, J1E+1))
     linhas = ['s_{}'.format(x) for x in linhas]
     
     colunas = list(range(1, J2E+1))
     colunas =['s_{}'.format(x) for x in colunas]
-        
-    payoff_array1 = np.random.ranf((J1E, J2E))
-    payoff_array1 = (maior - menor)*payoff_array1 + menor
-    payoff1 = pd.DataFrame(payoff_array1, index = linhas, columns = colunas)
     
-    payoff_array2 = np.random.ranf((J1E, J2E))
-    payoff_array2 = (maior - menor)*payoff_array2 + menor
-    payoff2 = pd.DataFrame(payoff_array2, index = linhas, columns = colunas)
+    # Criação do array aleatório com os payoffs de cada jogador
+    payoff_array = np.random.ranf((J1E, J2E))
+    payoff_array = (maior - menor)*payoff_array + menor
+    payoff = pd.DataFrame(payoff_array, index = linhas, columns = colunas).round(2)
     
-    if partial == 'J1':
-        representação = payoff1
+    return payoff
+
+def jogo(payoff1, payoff2):
+    '''
+    Parâmetros:
+    - payoff1: DataFrame contendo os payoffs do jogador 1;
+    - payoff2: DataFrame contendo os payoffs do jogador 2;
     
-    elif partial == 'J2':
-        representação = payoff2
+    Retorna a representação do jogo (com 2 jogadores, estratégias puras e finitas e um único período de ação);
+    '''
     
-    elif partial == 'jogo': 
-        payoff = payoff1.round(2).astype(str) +', '+ payoff2.round(2).astype(str)
-        representação = pd.DataFrame(payoff, index = linhas, columns = colunas)
-        
-    else:
-        representação = print('Argumento partial inválido. Tente não aplicá-lo ou aplique J1 ou J2')
+    representação = payoff1.astype(str) +', '+ payoff2.astype(str)
         
     return representação
+
+def solve(payoff1, payoff2):
+    '''
+    payoff1: DataFrame contendo os payoffs do jogador 1;
+    payoff2: DataFrame contendo os payoffs do jogador 2;
+    Obs: 'payoff1' e 'payoff2' devem se referir ao mesmo jogo;
+    
+    Retorna uma lista contendo os Equilíbrios de Nash do jogo determinado por 'payoff1' e 'payoff2';
+    '''
+    
+    def maximo(payoff, jogador):
+        
+        # Retorna um objeto que indica a linha ou coluna do maior valor para cada coluna ou cada linha, respectivamente.
+        
+        maximo = payoff.idxmax(axis=jogador-1)
+        maximo = maximo.reset_index()
+        maximo = np.array(maximo)
+        coluna1 = int((1-(-1)**jogador)/2)
+        coluna2 = int((1+(-1)**jogador)/2)
+        maximo = maximo[:,coluna1] +', '+ maximo[:,coluna2]
+        
+        return maximo
+    
+    solve1 = maximo(payoff1, 1)
+    solve2 = maximo(payoff2, 2)
+    
+    solve = np.intersect1d(solve1, solve2)
+    
+    return solve
+
+####### Só falta resolver um problema: se existirem valores máximos iguais entre os payoffs, as funções utilizadas pelo pandas e pelo numpy
+####### retornam somente a primeira estratégia em vez de retornar todas as estratégias que maximizam o payoff. Fora isso,
+####### parece estar correto. Teste no notebook
